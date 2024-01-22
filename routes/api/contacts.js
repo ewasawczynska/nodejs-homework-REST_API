78d595f0ac2501eb91b892f7d1fdc4e029ab6ffc
ewasawczynska <sawczynska.e@gmail.com>
2024-01-22 19:27:40 +0100

    [hw02-express] Add routes - part 1

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,16 +2,33 @@ const express = require('express')
 
 const router = express.Router()
 
+import { listContacts, getContactById, addContact } from '../../models/contacts.js'
+
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const allContacts = await listContacts();
+  return res.status(200).json(allContacts);
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params;
+  try {
+  const contacts = getContactById(contactId);
+  return res.status(200).json(contacts);
+  } catch {
+  res.status(404).json({message: 'Not found'});
+  };
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const body = req.body;
+  if (Object.keys(req.body).length === 0) {
+    res.status(400).json({ message: 'missing required field' });
+    return;
+  }
+  else {
+    const contact = await addContact(body);
+    res.status(201).json(contact)
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
