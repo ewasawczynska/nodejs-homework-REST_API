df2de4f04e335d5c57f6bce45612105d80edf216
ewasawczynska <sawczynska.e@gmail.com>
2024-01-30 22:47:05 +0100

    [hw03-mongodb] Try to fix

--- app.js
+++ app.js
@@ -8,17 +8,17 @@ const app = express();
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
 
-app.use('/api/contacts', contactsRouter)
+app.use('/db-contacts/contacts', contactsRouter)
 
-app.use((req, res) => {
+app.use((_, res, __) => {
   res.status(404).json({ message: 'Not found' })
 })
 
-app.use((err, req, res, next) => {
+app.use((err, _, res, __) => {
   res.status(500).json({ message: err.message })
 })
 
